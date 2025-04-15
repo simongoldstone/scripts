4926412fc7d6887740e0610c1f199a81a01e6625
Simon Goldstone <5681879+simongoldstone@users.noreply.github.com>
2025-04-15 17:17:52 +0100

    test hide

--- bad-usb/prank/gotchya/gotchya.ps1
+++ bad-usb/prank/gotchya/gotchya.ps1
@@ -1,1 +1,24 @@
 Write-Host "Gotchya!"
+[console]::beep()
+
+try {
+    $Async = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+    $Type = Add-Type -MemberDefinition $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
+    $hwnd = (Get-Process -PID $pid).MainWindowHandle
+    
+    if ($hwnd -ne [System.IntPtr]::Zero) {
+        $Type::ShowWindowAsync($hwnd, 0)
+    }
+    else {
+        $Host.UI.RawUI.WindowTitle = 'hidden'
+        $Proc = (Get-Process | Where-Object { $_.MainWindowTitle -eq 'hideme' })
+        $hwnd = $Proc.MainWindowHandle
+        $Type::ShowWindowAsync($hwnd, 0)
+    }
+} catch {
+    # unable to hide
+}
+
+
+Start-Sleep -Seconds 10
+[console]::beep()
